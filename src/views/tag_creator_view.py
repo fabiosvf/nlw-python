069d4d7479529ba8885db21f0c37cdeb081b61ff
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-02-11 14:13:55 -0300

    feat: Refactoring Code and Implementing Error Handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,9 +8,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
 
-        print("Estou na minha view")
-        print(http_request)
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        body = http_request.body
+        product_code = body["product_code"]
+
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
